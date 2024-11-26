28644b8f96d494d58c67ce3be5a501bc9bd07639
Mirela Oliveira <162753927+MichyOliveira@users.noreply.github.com>
2024-11-26 17:26:56 -0300

    Endreew Lima

---
+++ Site-Endreew/assets/scripts.js
@@ -0,0 +1,5 @@
+AOS.init({
+    duration: 1000,  // Duração da animação
+    easing: 'ease-in-out', // Tipo de transição
+    once: false,  // Se a animação ocorre uma vez ou em todas as rolagens
+});
